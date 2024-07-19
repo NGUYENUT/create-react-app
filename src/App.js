a8fa382c77087062a1010d449abdac4b0e0956d1
justin.nguyen <justin.nguyen@datadoghq.com>
2024-07-19 02:10:16 +0000

    pls

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import logo from './logo.svg';
+import { useState } from "react"
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null)
+
+  const callFlaskAPI =  () => {
+    const url = "flask-hello-world-teal-ten-86.vercel.app";
+    fetch(url,{
+      method: "GET",
+    }).then((res)=>{
+      console.log(res)
+      return res.text();
+    }).then((text)=>{
+     console.log(text)
+    })
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={() => callFlaskAPI()}>Click me for the FLASK API RESULT</button>
+
+        <p>
+          {data ? data : "no data"}
         </p>
         <a
           className="App-link"
